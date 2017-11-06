9f11a2248bfb133a7c9ae1f6eb14b75b7cf7076a
Lei Shi <sl9212@gmail.com>
2017-11-05 23:17:36 -0600

    thread func didn't get args, need debug

--- Project_3/xv6/user/threadtests.c
+++ Project_3/xv6/user/threadtests.c
@@ -16,7 +16,9 @@ main(int argc, char* argv[])
  stack = stack + (PGSIZE - (uint)stack % PGSIZE);
  }
 
- void* arg = NULL; // our DoThreadWork function is simple and doesn't need an arg
+ int* arg = malloc(sizeof(int)*2); // our DoThreadWork function is simple and doesn't need an arg
+ arg[0] = 1;
+ arg[1] = 2;
  int clone_pid = clone(DoThreadWork, arg, stack);
  clone_pid += 0;
  // the main thread of execution (aka parent process) continues executing here
@@ -30,6 +32,7 @@ main(int argc, char* argv[])
 void
 DoThreadWork(void* arg_ptr) {
  // clone creates a new thread, which begins execution here
- global = 5;
+ 
+ global = *((int*)arg_ptr) + *((int*)arg_ptr+1);
  exit();
 }
